ec07b260aecae1b09e7a55786449e166a1a4f6cc
Meijer <max1_meijer@hotmail.com>
2016-09-26 09:01:22 +0200

    s
    s

---
+++ AnimalShelter/AnimalShelter/Administration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelter
+{
+    class Administration
+    {
+        public List<Cat> Cats = new List<Cat>() { get; set;}
+        
+    }
+}
---
+++ AnimalShelter/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnimalShelter;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAddCat()
+        {         
+          
+        }
+    }
+}
